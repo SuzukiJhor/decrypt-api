b7c430fc4ffa3216e5cfd23d13d3654a3d127ba2
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-07-25 09:36:15 -0300

    feat: add lib cors to browser enable

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 import { decode } from './decrypt.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 const PORT = process.env.PORT || 3001;
 const privateKey = process.env.CRYPTOGRAPHY_PRIVATE_RSA_KEY;
 
